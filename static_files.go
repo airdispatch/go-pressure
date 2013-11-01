ef8c9dea3881944954aeaf6a56c5487bb92c5b17
Hunter Leath <h@hunterleath.com>
2013-11-01 15:59:20 -0400

    Fixed MIME Filetype Detection Error

--- static_files.go
+++ static_files.go
@@ -71,7 +71,7 @@ func (b StaticFileViewController) ContentLength() int {
 }
 
 func (b StaticFileViewController) ContentType() string {
-	return mime.TypeByExtension(strings.Split(b.Name, ".")[0])
+	return mime.TypeByExtension("." + strings.Split(b.Name, ".")[1])
 }
 
 func (b StaticFileViewController) Headers() ViewHeaders {
